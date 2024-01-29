de3e309d87ea280771c5cd7c6ed70e8dd92fe700
MumuXNYK <mumukshnayak2904@hotmail.com>
2024-01-29 12:06:02 +0530

    Sundown Studio

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
 
 var fixed = document.getElementById('fixed')
 var elems = document.querySelectorAll('.elem')
@@ -46,3 +46,21 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 10
   });
 
+
+var menu = document.querySelector('nav h3')
+var full = document.querySelector('#fullscr')
+var navimg = document.querySelector('nav img')
+var flag = 0
+
+menu.addEventListener('click',()=>{
+    if (flag == 0) {
+        full.style.top = 0
+        navimg.style.opacity = 0
+        flag = 1
+        
+    } else {
+        full.style.top = "-100%"
+        navimg.style.opacity = 1
+        flag = 0
+    }
+})
